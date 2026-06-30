90bea6c57541cb93dfd6f365f52f6a7da88bda5d
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-30 08:10:19 -0500

    Bloque revisión flujos: cargar auditor de flujo

--- BDLocal/adapters/bdl.screen-deps.js
+++ BDLocal/adapters/bdl.screen-deps.js
@@ -94,6 +94,7 @@
     "continuity/cont.index.js",
     "connections/shared/conn.audit.js",
     "connections/shared/conn.audit.screens.js",
+    "connections/shared/conn.flow.js",
     "adapters/bdl.screen-compat.js",
     "adapters/bdl.screen-autorefresh.js"
   ];
