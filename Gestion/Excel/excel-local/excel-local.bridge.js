4e419eeefa7952e81cfb4d26466ba36502e9be63
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-29 20:44:56 -0500

    Restaurar puente ExcelLocal para pantallas

---
+++ Gestion/Excel/excel-local/excel-local.bridge.js
@@ -0,0 +1,8 @@
+(function(window){
+  "use strict";
+  window.ExcelLocalBridge = window.ExcelLocalBridge || {
+    version:"bdlocal-compat",
+    ready:function(){ return true; },
+    syncFromBDLocal:function(){ return true; }
+  };
+})(window);
